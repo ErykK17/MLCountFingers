dbb0d8931c3b20ff5afce11c8d549c561e4d2e38
ErykK17 <erykrygier@gmail.com>
2024-08-20 14:36:06 +0200

    changed model to thresholds

--- get_data.py
+++ get_data.py
@@ -15,16 +15,13 @@ def load_data(data_dir):
             img = cv2.imread(img_path)
             img = cv2.resize(img, (128, 128))
             images.append(img)
-            if label[:-1] == 'Left':
-                label_int = 0 + int(label[-1])
-            else:
-                label_int = 6 + int(label[-1])  
+            label_int = int(label[-1])
             labels.append(label_int)
     images = np.array(images)
     labels = np.array(labels)
     return images, labels
 
-images, labels = load_data('C:/Users/erykr/OneDrive/Pulpit/MLFingerCount/MLCountFingers/Fingers')
+images, labels = load_data('C:/Users/erykr/OneDrive/Pulpit/MLFingerCount/MLCountFingers/FingersThresholded')
 
 images = images / 255.0
 
@@ -38,7 +35,7 @@ model = models.Sequential([
     layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
     layers.Dense(64, activation='relu'),
-    layers.Dense(12, activation='softmax')  # Wyjścia: 0, 1, 2, 3, 4, 5
+    layers.Dense(6, activation='softmax')  # Wyjścia: 0, 1, 2, 3, 4, 5
 ])
 
 
@@ -52,7 +49,7 @@ earlystopping = callbacks.EarlyStopping(monitor="val_loss",
                                         restore_best_weights=True)
 
 X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
-history = model.fit(X_train, y_train, epochs=50, validation_data=(X_test, y_test), callbacks=[earlystopping])
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_test, y_test), callbacks=[earlystopping])
 
 test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
 print(f'\nTest accuracy: {test_acc}')
--- test_model.py
+++ test_model.py
@@ -2,22 +2,27 @@ import cv2
 import numpy as np
 from keras import models
 
-model = models.load_model('model.keras')
+
+image = cv2.imread('TestImages/5T.jpg')
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-def predict_fingers(image_path):
-    img = cv2.imread(image_path)
+_, threshold = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY)
+
+model = models.load_model('modelthresholded.keras')
+
+
+def predict_fingers(img):
     img = cv2.resize(img, (128, 128))
+    img = np.stack((img,) * 3, axis=-1)  # Convert to 3 channels
     img = np.expand_dims(img, axis=0) / 255.0
     prediction = model.predict(img)
     print(prediction)
     return np.argmax(prediction)
 
-# Przykład predykcji
-image_path = 'C:/Users/erykr/OneDrive/Pulpit/MLFingerCount/MLCountFingers/TestImages/0.jpg'
-img = cv2.imread(image_path)
-img = cv2.resize(img, (128, 128))
+img = cv2.resize(threshold, (128, 128))
 cv2.imshow("idk",img)
 cv2.waitKey(3000)
 cv2.destroyAllWindows()
-print(f'Predicted number of fingers: {predict_fingers(image_path)}')
+print(f'Predicted number of fingers: {predict_fingers(threshold)}')
